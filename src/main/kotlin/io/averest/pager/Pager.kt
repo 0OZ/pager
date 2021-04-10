f14c2aef5b43dfcfbbaed412f764399e63934af1
Christian <matschke.berlin@icloud.com>
2021-04-10 16:19:48 +0200

    refactor

--- src/main/kotlin/io/averest/pager/Pager.kt
+++ src/main/kotlin/io/averest/pager/Pager.kt
@@ -3,13 +3,12 @@ package io.averest.pager
 import io.averest.pager.valueObject.PaginationIdentifier
 
 open class Pager(
-    private val url: String,
+    private val path: String,
     pagesIdentifiers: PaginationIdentifier = PaginationIdentifier("\"?page\"?|seite|pageNumber"),
+    private val pageRegex: Regex = Regex("(?i)(.+(${pagesIdentifiers.identifier})([=:]))\"?(\\d{1,4})\"?(.+|\$)")
 ) {
-    private val pageRegex = Regex("(?i)(.+(${pagesIdentifiers.identifier})([=:]))\"?(\\d{1,4})\"?(.+|\$)")
-
     init {
-        if (!url.contains(Regex("(?i)${pagesIdentifiers.identifier}"))) throw Exception("missing page structure")
+        if (!path.contains(Regex("(?i)${pagesIdentifiers.identifier}"))) throw Exception("unknown page structure")
     }
 
     fun incrementPage(): String {
@@ -18,13 +17,13 @@ open class Pager(
     }
 
     fun removeIndexFromUrl() =
-        url.replace(pageRegex, "$1$5")
+        path.replace(pageRegex, "$1$5")
 
     fun replacePage(index: Int) =
-        url.replace(pageRegex, "$1$index$5")
+        path.replace(pageRegex, "$1$index$5")
 
     fun extractPageIndex(): Int {
-        val currentPageIndex = url.replace(pageRegex, "$4")
+        val currentPageIndex = path.replace(pageRegex, "$4")
         return currentPageIndex.toInt()
     }
 }
--- src/main/kotlin/io/averest/pager/aggregate/OffsetPager.kt
+++ src/main/kotlin/io/averest/pager/aggregate/OffsetPager.kt
@@ -1,17 +1,18 @@
-package io.averest.pager.aggregator
+package io.averest.pager.aggregate
 
 import io.averest.pager.Pager
 import io.averest.pager.valueObject.PaginationIdentifier
 
 class OffsetPager(
-    private val url: String,
+    private val path: String,
     pagesIdentifiers: PaginationIdentifier = PaginationIdentifier("offset")
-) : Pager(url, pagesIdentifiers) {
-    private fun addPageSize(): Int{
-        val pageSize = url.replace(Regex(".+pageSize=(\\d+?)"),"$1")
+) : Pager(path, pagesIdentifiers) {
+    private val pageSizeRegex = Regex(".+pageSize=(\\d+?)")
+
+    private fun addPageSize(): Int {
+        val pageSize = path.replace(pageSizeRegex, "$1")
         return pageSize.toInt()
     }
-
 
     fun incrementOffset(): String {
         val currentPageIndex = extractPageIndex()
